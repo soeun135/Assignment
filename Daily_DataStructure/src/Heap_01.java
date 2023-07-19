1ca33d528c58e5bdf8d7c2f274b74f004c1534ca
soeuns <soensuckun@naver.com>
2023-07-20 00:06:41 +0900

    자료구조 6일차 Head 필수문제 시도 중

--- Daily_DataStructure/src/Heap_01.java
+++ Daily_DataStructure/src/Heap_01.java
@@ -1,31 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Heap_01 {
-    public static void heapify(int A[],int k,int n) {
+    static int count = 0;
+    static int K = 0;
+    public static void swap(int A[], int a, int b){
+        int tmp = A[a];
+        A[a] = A[b];
+        A[b] = tmp;
+        count ++;
+
+        if(K == count){
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < A.length; i++) {
+                sb.append(A[i]).append(" ");
+            }
+            System.out.println(sb.toString());
+            System.exit(0);
+        }
+    }
+    public static void heapSort(int A[]){
+        int n = A.length - 1;
+        buildMinHeap(A, n);
+        for (int i = n; i >= 2 ; i--) {
+            swap(A, 1, i);
+            heapify(A, 1, i-1);
+        }
+    }
+    public static void buildMinHeap(int A[],int n){
+        for (int i = n / 2; i >= 1 ; i--) {
+            heapify(A, i, n);
+        }
+    }
+    public static void heapify(int []A,int k,int n) {
         int left = 2 * k;
         int right = 2 * k + 1;
-        int smaller = 0;
-        if (right < n) {
-            if (A[left] < A[right]) {
-                smaller = left;
-            }
-            else{
-                smaller = right;
-            }
-        }
-        else if (left <= n) {
-            smaller = left;
-        }
-        else return;
+        int smaller = -1;
 
-    //최소 힙 성질을 만족하지 못하는 경우 재귀적으로 수정한다.
+        if (right <= n) {
+            smaller = A[left] < A[right] ? left : right;
+        } else if (left <= n) {
+            smaller = left;
+        } else return;
+
+
         if (A[smaller] < A[k]) {
-            int temp = A[smaller];
-            A[smaller] = A[k];
-            A[k] = temp;
+            swap(A, smaller, k);
             heapify(A, smaller, n);
         }
     }
@@ -34,15 +57,14 @@ public class Heap_01 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int A[] = new int[N];
+        K = Integer.parseInt(st.nextToken());
+        int A[] = new int[N+1];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < N+1; i++) {
+           A[i] = Integer.parseInt(st.nextToken());
         }
-        heapify(A,K,N);
-        for(int i : A){
-            System.out.print(i+" ");
-        }
+        heapSort(A);
+        System.out.println(-1);
     }
 }
---
+++ Daily_DataStructure/src/Heap_02.java
@@ -0,0 +1,11 @@
+public class Heap_02 {
+    public static int solution(int[][] jobs) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String args[]){
+        int [][]arr = {{0,3},{1,9},{2,6}};
+        System.out.println(solution(arr));
+    }
+}
